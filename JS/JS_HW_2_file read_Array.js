461175a7e2d82362ee7375559b96cdcf0e98487b
EvSvirch <103583065+EvSvirch@users.noreply.github.com>
2022-10-01 03:36:00 +0200

    Create JS_HW_2_file read_Array.js

---
+++ JS/JS_HW_2_file read_Array.js
@@ -0,0 +1,22 @@
+// There is a file with arrays of JSON objects. Each object is an identification card of a person. 
+// We only need to store unique values in this array. Implement a function that will do the job.
+
+const fs = require('fs')                    
+let file = fs.readFileSync('JS_HW_2_Array.json')    
+let personFile = JSON.parse(file)          
+
+function person (person) {
+    for (i = 0; i < person.length - 1; i++) {             
+        for (i2 = person.length - 1; i2 > i; i2--) {      
+            if (person[i].username == person[i2].username && person[i].email == person[i2].email) {   
+                person.splice(i2,1)
+            }
+        }
+    }
+
+    for (k1 = 0; k1 < person.length; k1++) {               
+        console.log(person[k1].name, person[k1].username)
+    }
+}
+
+person(personFile)
